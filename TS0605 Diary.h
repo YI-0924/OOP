bfdd6aa789abe2497e8c67b1ef1a0bda3775c000
YI <85835199+YI-0924@users.noreply.github.com>
2022-03-28 22:21:29 +0800

    Create TS0605 Diary.h

---
+++ TS0605 Diary.h
@@ -0,0 +1,20 @@
+//Name:呂欣怡
+//Date:March 27,2022
+//Last Update:March 27,2022
+//Problem statement:Observation diary.
+
+#include<iostream>
+
+using namespace std;
+
+class Diary {
+public:
+	string currentDate;
+
+	//Set the current date code(A string of words.May not all necessarily be numbers.)
+	static void NewDay(string day);
+	
+};
+
+
+
