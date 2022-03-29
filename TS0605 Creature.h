d6fa9d70b52e90cadb2a120626c066e31d97fd1d
YI <85835199+YI-0924@users.noreply.github.com>
2022-03-29 19:54:49 +0800

    Update TS0605 Creature.h

--- TS0605 Creature.h
+++ TS0605 Creature.h
@@ -10,8 +10,9 @@ using namespace std;
 class Creature {
 public:
 	string creatureName;
-	int bodyPartNum;
-	string bodyPartName;
+	string *bodyPartName;
+	int* bodyPartNum;
+	int count = 0;
 
 	//Contains a creature named name.
 	Creature(string name);
@@ -26,17 +27,17 @@ public:
 
 	//For example: c["leg"] = 3: set the number of body parts in Creature c named "leg" to 3 
 	//and record "sample's leg appeared (0 -> 3)" in the log. 
-	int operator=(Creature c);
+	void operator=(Creature c);
 
 	//For example: c["leg"] += 2: the number of body parts named "leg" increases by 2,
 	//so the number of body parts named "leg" becomes 5.
 	//And please record "sample's leg increased (3 -> 5)" in the log.
-	int operator+=(Creature c);
+	void operator+=(Creature c);
 
 	//For example: c["leg"] -= 2: the number of body parts named "leg" decreases by 2, 
 	//so the number of body parts named "leg" becomes 3.
 	//And please record "sample's leg decreased (5 -> 3)" in the log.
-	int operator-=(Creature c);
+	void operator-=(Creature c);
 
 
 
