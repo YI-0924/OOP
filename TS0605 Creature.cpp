104836a03e26d05528dfedf9abb4f6acd9ca5ae1
YI <85835199+YI-0924@users.noreply.github.com>
2022-03-29 19:54:33 +0800

    Update TS0605 Creature.cpp

--- TS0605 Creature.cpp
+++ TS0605 Creature.cpp
@@ -12,43 +12,57 @@ Creature::Creature(string name, Creature base)
 	creatureName = name;
 	bodyPartName = base.bodyPartName;
 	bodyPartNum = base.bodyPartNum;
+	count = base.count;
 }
 
 //For example: c["leg"]: return to the body part of Creature c named "leg".
 string Creature::operator[](Creature c)
 {
-	return c.bodyPartName;
+	int find = -1;
+	for (int i = 0; i < count; i++) {
+		if (bodyPartName != c.bodyPartName) {
+
+		}
+		else {
+			find = i;
+		}
+	}
+	if (find != -1) {
+		return c.bodyPartName[find];
+	}
+	
 }
 
 //For example: c["leg"] = 3: set the number of body parts in Creature c named "leg" to 3 
 //and record "sample's leg appeared (0 -> 3)" in the log. 
-int Creature::operator=(Creature c)
+void Creature::operator=(Creature c)
 {
-	if (bodyPartName == c.bodyPartName) {
-		return bodyPartNum;
+	while (bodyPartName != c.bodyPartName) {
+		bodyPartName++;
 	}
+	bodyPartNum[count] = c.bodyPartNum[count];
 }
 
 //For example: c["leg"] += 2: the number of body parts named "leg" increases by 2,
 //so the number of body parts named "leg" becomes 5.
 //And please record "sample's leg increased (3 -> 5)" in the log.
-int Creature::operator+=(Creature c)
+void Creature::operator+=(Creature c)
 {
-	if (bodyPartName == c.bodyPartName) {
-		bodyPartNum += c.bodyPartNum;
-		return bodyPartNum;
+	while (bodyPartName != c.bodyPartName) {
+		bodyPartName++;
 	}
+	bodyPartNum[count] += c.bodyPartNum[count];
 }
 
 //For example: c["leg"] -= 2: the number of body parts named "leg" decreases by 2, 
 //so the number of body parts named "leg" becomes 3.
 //And please record "sample's leg decreased (5 -> 3)" in the log.
-int Creature::operator-=(Creature c)
+void Creature::operator-=(Creature c)
 {
-	if (bodyPartName == c.bodyPartName) {
-		bodyPartNum -= c.bodyPartNum;
-		return bodyPartNum;
+	while (bodyPartName != c.bodyPartName) {
+		bodyPartName++;
 	}
+	bodyPartNum[count] -= c.bodyPartNum[count];
 }
 
 
@@ -56,7 +70,10 @@ int Creature::operator-=(Creature c)
 //Prints the value of each body part of the organism.
 void Creature::PrintStatus()
 {
-
+	cout << creatureName << "'s status:\n";
+	for (int i = 0; i < count; i++) {
+		cout << bodyPartName[i] << "*" << bodyPartNum[i] << endl;
+	}
 }
 
 //Prints the log information of the creature on a Diary basis since it was collected.
