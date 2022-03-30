ce5f8262355b7ef6ca35284982fd070c38ba8017
YI <85835199+YI-0924@users.noreply.github.com>
2022-03-30 10:28:20 +0800

    Update TS0605 Creature.h

--- TS0605 Creature.h
+++ TS0605 Creature.h
@@ -1,6 +1,6 @@
 //Name:呂欣怡
 //Date:March 27,2022
-//Last Update:March 27,2022
+//Last Update:March 29,2022
 //Problem statement:Observation diary.
 
 #include<iostream>
@@ -23,21 +23,21 @@ public:
 
 
 	//For example: c["leg"]: return to the body part of Creature c named "leg".
-	string operator[](Creature c);
+	Creature operator[](string name);
 
 	//For example: c["leg"] = 3: set the number of body parts in Creature c named "leg" to 3 
 	//and record "sample's leg appeared (0 -> 3)" in the log. 
-	void operator=(Creature c);
+	Creature operator=(int num);
 
 	//For example: c["leg"] += 2: the number of body parts named "leg" increases by 2,
 	//so the number of body parts named "leg" becomes 5.
 	//And please record "sample's leg increased (3 -> 5)" in the log.
-	void operator+=(Creature c);
+	Creature operator+=(int num);
 
 	//For example: c["leg"] -= 2: the number of body parts named "leg" decreases by 2, 
 	//so the number of body parts named "leg" becomes 3.
 	//And please record "sample's leg decreased (5 -> 3)" in the log.
-	void operator-=(Creature c);
+	Creature operator-=(int num);
 
 
 
