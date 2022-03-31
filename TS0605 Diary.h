faf2aae988a4ec40a5c9525b0357c2ab0c334e1c
YI <85835199+YI-0924@users.noreply.github.com>
2022-03-31 11:28:05 +0800

    Update TS0605 Diary.h

--- TS0605 Diary.h
+++ TS0605 Diary.h
@@ -1,19 +1,31 @@
 //Name:呂欣怡
 //Date:March 27,2022
-//Last Update:March 27,2022
+//Last Update:March 29,2022
 //Problem statement:Observation diary.
 
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
+typedef struct logData {
+	string name;
+	string log;
+}logData;
+
+typedef struct dateLog {
+	string date;
+	vector<logData> content;
+}dateLog;
+
 class Diary {
 public:
-	string currentDate;
+	static vector<dateLog> logList;
 
 	//Set the current date code(A string of words.May not all necessarily be numbers.)
 	static void NewDay(string day);
-	
+
+	static void recordLog(string inName, string inContent);
 };
 
 
