c98ff2402dde93ed5e0bacb548b6d921491c25e6
YI <85835199+YI-0924@users.noreply.github.com>
2022-03-31 11:27:49 +0800

    Update TS0605 Diary.cpp

--- TS0605 Diary.cpp
+++ TS0605 Diary.cpp
@@ -1,7 +1,24 @@
+//Name:呂欣怡
+//Date:March 27,2022
+//Last Update:March 29,2022
+//Problem statement:Observation diary.
+
 #include"Diary.h"
+
+vector<dateLog> Diary::logList;
 
 //Set the current date code(A string of words.May not all necessarily be numbers.)
 void Diary::NewDay(string day)
 {
-	
+	dateLog newDay;
+	newDay.date = day;
+	logList.push_back(newDay);
+}
+
+void Diary::recordLog(string inName, string inContent) 
+{
+	logData nameLog;
+	nameLog.name = inName;
+	nameLog.log = inContent;
+	logList[logList.size() - 1].content.push_back(nameLog);
 }
